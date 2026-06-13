c397af735b09d1fca4b949683729d88af2a84e92
Devansh <dvbittu.pandey@gmail.com>
2026-06-13 18:08:53 +0530

    Delete Feature Added in TODO APP

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -2,9 +2,22 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #add task feature
     path('addTask/',views.addTask,name='addTask'),
+
+    #mark as done url pattern 
     path('completeTask/<int:pk>/',views.completeTask,name='completeTask'),
+
+    # makr as undone feature 
     path('markAsUnDone/<int:pk>/',views.markAsUnDone,name='markAsUnDone'),
+
+    #Edit Task Feature 
+    path('edit_task/<int:pk>/',views.edit_task,name='edit_task'),
+
+    # Delete URL pattern to delete the task 
+    path('delete_task/<int:pk>/',views.delete_task,name='delete_task'),
+
+     
 ]
 
 
